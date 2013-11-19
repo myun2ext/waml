97ffedd4ca81c16b76d9e804a779ebf33f498d15
myun2 <myun2@nwhite.info>
2013-11-19 17:09:53 +0900

    document.hpp Add

---
+++ include/myun2/waml/document.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_myun2__waml__document_HPP__
+#define __github_com_myun2__waml__document_HPP__
+
+namespace myun2
+{
+	namespace waml
+	{
+		class document
+		{
+		};
+	}
+}
+
+#endif//__github_com_myun2__waml__document_HPP__
