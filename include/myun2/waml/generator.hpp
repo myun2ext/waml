1dafec9d69fe2eb79f6c7254619114e23114b407
myun2 <myun2@nwhite.info>
2013-11-19 17:09:30 +0900

    generator.hpp Add

---
+++ include/myun2/waml/generator.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_myun2__waml__generator_HPP__
+#define __github_com_myun2__waml__generator_HPP__
+
+namespace myun2
+{
+	namespace waml
+	{
+		class generator
+		{
+		};
+	}
+}
+
+#endif//__github_com_myun2__waml__generator_HPP__
