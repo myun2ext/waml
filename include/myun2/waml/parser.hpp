8e013a3ec9fb01a4ab63c604a594357da477487e
myun2 <myun2@nwhite.info>
2013-11-19 17:08:12 +0900

    myun2/waml/parser.hpp Add

---
+++ include/myun2/waml/parser.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_myun2__waml__parser_HPP__
+#define __github_com_myun2__waml__parser_HPP__
+
+namespace myun2
+{
+	namespace waml
+	{
+		class parser
+		{
+		};
+	}
+}
+
+#endif//__github_com_myun2__waml__parser_HPP__
